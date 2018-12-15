65736369b9e2351f807e488a9e3c914470afec0c
Carlos Dominguez <dominguerilla@gmail.com>
2018-12-15 16:19:56 -0500

    fading to black

--- Assets/Scripts/SceneTransitioner.cs
+++ Assets/Scripts/SceneTransitioner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Executes the transition between the field and a battle.
@@ -9,31 +10,109 @@ using UnityEngine;
 public class SceneTransitioner : MonoBehaviour {
     
     public Transform scenePosition;
+    public Image battleFadeImage;
+    [SerializeField]
+    float fadeSpeed = 1.0f;
 
     GameObject activeBattleScene;
+    Camera battleCam, returnCam;
+    Light battleLight, returnLight;
 
-    public void CreateBattleScene(Combatant[] leftParty, Combatant[] rightParty, GameObject battleScene){
+    public void CreateBattleScene(Combatant[] leftParty, Combatant[] rightParty, GameObject battleScene, Camera returnCam = null, Light returnLight = null){
+        StartCoroutine(InitializeBattleScene(leftParty, rightParty, battleScene, returnCam, returnLight));
+    }
+
+    // TODO Make it so that it initializes a battle scene that already exists in the Scene.
+    // That way, we don't have to instantiate a new one every battle
+    IEnumerator InitializeBattleScene(Combatant[] leftParty, Combatant[] rightParty, GameObject battleScene, Camera returnCam = null, Light returnLight = null){
+        yield return StartCoroutine(FadeImage(1.0f));
         GameObject newScene = Instantiate(battleScene, scenePosition.position, scenePosition.rotation) as GameObject;
         activeBattleScene = newScene;
 
         GameObject leftPartyParent = newScene.transform.Find("LeftParty").gameObject;
         GameObject rightPartyParent = newScene.transform.Find("RightParty").gameObject;
+        
+        // Initialize camera
+        this.battleCam = newScene.GetComponentInChildren<Camera>();
+        this.battleCam.enabled = true;
+        this.returnCam = returnCam;
+        if(this.returnCam){
+            this.returnCam.enabled = false;
+        }
 
-        Debug.Log("Left positions");
+        // Initialize lights
+        // Assumes there's only one light in battleScene, and one return light
+        this.battleLight = newScene.GetComponentInChildren<Light>();
+        if(this.battleLight){
+            this.battleLight.enabled = true;
+        }
+        this.returnLight = returnLight;
+        if(this.returnLight){
+            this.returnLight.enabled = false;
+        }
+
+        yield return StartCoroutine(FadeImage(0.0f));
         Transform[] leftPositions = GetPositions(leftPartyParent);
         SpawnParty(leftParty, leftPositions, leftPartyParent.transform);
 
-        Debug.Log("Right positions");
         Transform[] rightPositions = GetPositions(rightPartyParent);
         SpawnParty(rightParty, rightPositions, rightPartyParent.transform);
+        yield return null;
     }
 
     public void DestroyBattleScene(){
-        Destroy(activeBattleScene);
-        activeBattleScene = null;
+        StartCoroutine(TakeDownBattleScene());
     }
 
-    public Transform[] GetPositions(GameObject parentObj){
+    IEnumerator TakeDownBattleScene(){
+        yield return StartCoroutine(FadeImage(1.0f));
+        if(returnCam){
+            returnCam.enabled = true;
+            returnCam = null;
+        }
+        if(returnLight){
+            returnLight.enabled = true;
+            returnLight = null;
+        }
+
+        if(battleCam){
+            battleCam.enabled = false;
+            battleCam = null;
+        }
+
+        if(battleLight){
+            battleLight.enabled = false;
+            battleLight = null;
+        }
+
+        Destroy(activeBattleScene);
+        activeBattleScene = null;
+        yield return StartCoroutine(FadeImage(0.0f));
+        yield return null;
+    }
+
+    IEnumerator FadeImage(float alpha){
+        if(battleFadeImage != null){
+            if(battleFadeImage.color.a < alpha){
+                while(battleFadeImage.color.a < alpha){
+                    var tempColor = battleFadeImage.color;
+                    tempColor.a += Time.deltaTime * fadeSpeed;
+                    battleFadeImage.color = tempColor;
+                    yield return new WaitForEndOfFrame();
+                }
+            }else if(battleFadeImage.color.a > alpha){
+                while(battleFadeImage.color.a > alpha){
+                    var tempColor = battleFadeImage.color;
+                    tempColor.a -= Time.deltaTime * fadeSpeed;
+                    battleFadeImage.color = tempColor;
+                    yield return new WaitForEndOfFrame();
+                }
+
+            }
+        }
+    }
+
+    Transform[] GetPositions(GameObject parentObj){
         List<Transform> positions = new List<Transform>();
         foreach (Transform t in parentObj.transform){
             if(t.gameObject.name.StartsWith("Position")){
@@ -43,7 +122,7 @@ public class SceneTransitioner : MonoBehaviour {
         return positions.ToArray();
     }
 
-    public void SpawnParty(Combatant[] party, Transform[] positions, Transform parent = null){
+    void SpawnParty(Combatant[] party, Transform[] positions, Transform parent = null){
         for(int i = 0; i < party.Length; i++){
             if(i >= positions.Length){
                 break;
