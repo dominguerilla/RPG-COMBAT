62023f173eb7e4a7507a58fc0f47b621d07a1b51
Carlos Dominguez <dominguerilla@gmail.com>
2019-01-09 22:20:32 -0500

    CameraController testing

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,7 +7,9 @@ public class CameraController : MonoBehaviour {
     private static CameraController _instance;
     public static CameraController Instance { get {return _instance; } }
 
+    
     Camera mainCamera, currentCamera;
+    bool cameraIsMoving;
 
     private void Awake() {
         if(_instance != null && _instance != this){
@@ -16,7 +18,8 @@ public class CameraController : MonoBehaviour {
             _instance = this;
         }
 
-        mainCamera = Camera.main;    
+        mainCamera = Camera.main;
+        currentCamera = mainCamera;
     }
     
     /// <summary>
@@ -41,4 +44,22 @@ public class CameraController : MonoBehaviour {
         this.currentCamera.enabled = true;
     }
 
+    public void MoveCamera(Vector3 newLocation, float camSpeed = 5.0f){
+        if(!cameraIsMoving){
+            StartCoroutine(LerpCamera(newLocation, camSpeed));
+        }
+    }
+
+    public Camera GetActiveCamera(){
+        return this.currentCamera;
+    }
+
+    IEnumerator LerpCamera(Vector3 newLocation, float camSpeed){
+        cameraIsMoving = true;
+        while(currentCamera.transform.position != newLocation){
+            currentCamera.transform.position = Vector3.Lerp(currentCamera.transform.position, newLocation, Time.deltaTime * camSpeed);
+            yield return null;
+        }
+        cameraIsMoving = false;
+    }
 }
---
+++ Assets/Scripts/Testing/CameraTesterEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraTester))]
+public class CameraTesterEditor : Editor{
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        CameraTester ct = (CameraTester)target;
+        if(Application.isPlaying){
+            if(GUILayout.Button("Zoom In")){
+                ct.ZoomIn();
+            }
+            if(GUILayout.Button("Zoom Out")){
+                ct.ZoomOut();
+            }
+        }
+    }
+}
