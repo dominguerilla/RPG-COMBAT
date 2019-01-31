8517a543cb785e0075fe1d63288910295e472342
cdom <dominguerilla@gmail.com>
2019-01-31 17:46:02 -0500

    added unit test for equipment

---
+++ Assets/Editor/EquipmentTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using LIMB;
+
+public class EquipmentTest {
+
+    // attacking, equipping a sword to a hand, attacking again
+    [Test]
+    public void SwordEquipAndAttack() {
+        Assert.IsTrue(false);
+    }
+
+    // being attacked, equipping armor, being attacked again
+    [Test]
+    public void ArmorEquipAndDefend() {
+        Assert.IsTrue(false);
+    }
+
+    // shield that blocks all but 1 damage
+    [Test]
+    public void SuperShield() {
+        Assert.IsTrue(false);
+    }
+}
