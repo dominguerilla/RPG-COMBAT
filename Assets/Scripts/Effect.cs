0974f7f3c040be0d0954d14c79ea18b565c782e8
cdom <dominguerilla@gmail.com>
2019-01-31 18:26:56 -0500

    updated effect comment

--- Assets/Scripts/Effect.cs
+++ Assets/Scripts/Effect.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace LIMB {
+    /// <summary>
+    /// You can consider an Effect a collection of Buffs, Damage, and a duration in which they take effect
+    /// </summary>
     [CreateAssetMenu(fileName = "New Effect", menuName = "Effect", order = 53)]
     public class Effect : ScriptableObject {
         public string Name;
