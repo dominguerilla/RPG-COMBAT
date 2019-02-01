3b713a4a370e93b312d5e41ccbf90e53bd648a44
cdom <dominguerilla@gmail.com>
2019-01-31 21:17:51 -0500

    basic Equip, GetBuffedStatus, GetStat

--- Assets/Scripts/Limb.cs
+++ Assets/Scripts/Limb.cs
@@ -9,12 +9,49 @@ namespace LIMB {
 
         [Tooltip("Uses the combatant's base stats by default.")]
         public List<StatValue> limbStats;
+        public List<Equipment> equipment;
+
+        static float DEFAULT_STAT = 1f;
 
         public Limb() { }
 
         public Limb(string name, params StatValue[] limbStats) {
             this.name = name;
             this.limbStats = new List<StatValue>(limbStats);
+            equipment = new List<Equipment>();
         }
+
+        public void Equip(Equipment equipment) {
+            this.equipment.Add(equipment);
+        }
+
+        public float GetBuffedStatus(Stats.STAT stat, float baseStat) {
+            float totalStats = baseStat + GetStat(stat);
+
+            List<StatBuff> buffs = new List<StatBuff>();
+            float percentageDelta = 0f;
+            foreach(Equipment equip in equipment) {
+                List<StatBuff> equipBuffs = equip.GetBuffs(stat);
+                foreach(StatBuff equipBuff in equipBuffs) {
+                    // collect percentages total of all buffs
+                    // ex. +10% DEF glove +5% DEF ring -3% DEF wound = +12% DEF
+                    // percentageDelta
+                }
+            }
+
+            float delta = .01f * percentageDelta * totalStats;
+            return totalStats + delta;
+        }
+        
+
+        public float GetStat(Stats.STAT stat) {
+            // TODO duplication in CombatantData.GetStat()
+            if (limbStats.Exists(x => x.Stat == stat)) {
+                return limbStats.Find(x => x.Stat == stat).Value;
+            }else {
+                return DEFAULT_STAT;
+            }
+        }
+
     }
 }
